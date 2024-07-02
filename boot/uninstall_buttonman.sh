5e4c25546c395c787b52cae666737273960d37a4
kenblu24 <kenblu24@hotmail.com>
2024-07-02 15:55:24 -0400

    update battchk with quiet modes

--- boot/uninstall_buttonman.sh
+++ boot/uninstall_buttonman.sh
@@ -5,6 +5,14 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
+BASHRC='/home/pi/.bashrc'
+
+function removefrom_bashrc {
+    LINE=$1
+    grep -v "$LINE" $BASHRC > temp && mv temp $BASHRC
+    return
+}
+
 echo 'Removing symlink /etc/systemd/system/buttonman.service'
 systemctl stop buttonman.service
 systemctl disable buttonman.service
@@ -12,4 +20,6 @@ rm /etc/systemd/system/buttonman.service
 echo 'Enabling hw_button_scan.service'
 systemctl enable hw_button_scan.service
 systemctl start hw_button_scan.service
+echo 'Removing aliases from bashrc'
+removefrom_bashrc "alias batt='sudo python3 /home/pi/boot/battchk.py'"
 echo 'Done'
