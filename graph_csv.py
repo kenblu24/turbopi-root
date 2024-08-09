00b222073b89567141c04356d937dc04da4f3e06
Logan <lpr1m3@icloud.com>
2024-08-09 13:16:31 -0400

    Fixed errors with incorrect columns and added legend

--- graph_csv.py
+++ graph_csv.py
@@ -7,10 +7,10 @@ class Graph():
     def graph(self, filename):
         plt.rcParams["figure.figsize"] = [7.00, 3.50]
         plt.rcParams["figure.autolayout"] = True
-        columns = ["Name", "Marks"]
-        df = pd.read_csv(filename, usecols=columns)
-        print("Contents in csv file:", df)
-        plt.plot(df.Name, df.Marks)
+        
+        df = pd.read_csv(filename)
+        df.T.plot(kind='line')
+        plt.legend(title='Variables', bbox_to_anchor=(1.05, 1), loc='upper left')
         plt.show()
 
 
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="csv file to be graphed")
     filename = parser.parse_args().filename
-    breakpoint()
-    graph = Graph(filename)
-    graph.graph()
+    # breakpoint()
+    graph = Graph()
+    graph.graph(filename)
